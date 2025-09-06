f57c91e108873d55f08769ac8139a8f57398a886
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-09-06 21:19:02 +0200

    Updates

---
+++ _next/static/XN_d5285_WyhEuxzYQrwI/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/96b6094348d1d902.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/89b415ee9347fba0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/419352f452740800.js",
+  "static/chunks/turbopack-a63d3c9ae1a12b19.js"
+])
