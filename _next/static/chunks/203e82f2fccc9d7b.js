4d5b9d1cb66f6d3b507ad251bce9395247b3d13d
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-09-23 13:27:16 +0200

    Updates

---
+++ _next/static/chunks/203e82f2fccc9d7b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/6e05d9ffe3b1b768.js",
+  "static/chunks/turbopack-e560211b0ac9617d.js"
+])
---
+++ _next/static/chunks/89360130b9352683.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/6e05d9ffe3b1b768.js",
+  "static/chunks/turbopack-d33fd589ced4894c.js"
+])
